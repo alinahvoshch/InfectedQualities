d7365f656e20e3e648711c6443e01590ca57f9f3
Mellohi <mallohi138@gmail.com>
2024-12-22 22:24:22 +0300

    Update key of naught spawning
    Now is 90% based on vanilla code instead of confection

--- Core/InfectedQualitiesPlayer.cs
+++ Core/InfectedQualitiesPlayer.cs
@@ -7,81 +7,70 @@ namespace InfectedQualities.Core
 {
     public class InfectedQualitiesPlayer : ModPlayer
     {
-        //Most of the code for the key of naught was taken from the Confection's source code, credits belong to their respective owners
         private int LastChest = -1;
 
         public override void PreUpdateBuffs()
         {
             if (Main.netMode != NetmodeID.MultiplayerClient)
             {
-                if (Main.hardMode && Player.chest == -1 && LastChest >= 0 && Main.chest[LastChest] != null)
+                if (Main.hardMode && Player.chest == -1 && LastChest >= 0)
                 {
-                    int x = Main.chest[LastChest].x;
-                    int y = Main.chest[LastChest].y;
-
-                    int num = Chest.FindChest(x, y);
-
-                    if (num != -1)
+                    Chest lastChest = Main.chest[LastChest];
+                    if (lastChest != null && Chest.FindChest(lastChest.x, lastChest.y) != 0)
                     {
-                        int key = 0;
-                        int other = 0;
-
-                        ushort type = Main.tile[Main.chest[num].x, Main.chest[num].y].TileType;
-                        int tileFrame = Main.tile[Main.chest[num].x, Main.chest[num].y].TileFrameX / 36;
-
-                        if (TileID.Sets.BasicChest[type] && (tileFrame < 5 || tileFrame > 6))
+                        int x = lastChest.x;
+                        int y = lastChest.y;
+                        int keyOfNaught = 0;
+                        bool other = false;
+                        if (TileID.Sets.BasicChest[Main.tile[x, y].TileType] && (Main.tile[x, y].TileFrameX / 36 < 5 || Main.tile[x, y].TileFrameX / 36 > 6))
                         {
                             for (int i = 0; i < 40; i++)
                             {
-                                if (Main.chest[num].item[i].type == ModContent.ItemType<KeyOfNaught>())
+                                if (lastChest.item[i].type == ModContent.ItemType<KeyOfNaught>())
                                 {
-                                    key += Main.chest[num].item[i].stack;
+                                    keyOfNaught += lastChest.item[i].stack;
                                 }
-                                else if (Main.chest[num].item[i].type > ItemID.None)
+                                else if (lastChest.item[i].type != ItemID.None)
                                 {
-                                    other++;
+                                    other = true;
+                                    break;
                                 }
                             }
                         }
 
-                        if (other == 0 && key == 1)
+                        if (keyOfNaught == 1 && !other)
                         {
                             if (TileID.Sets.BasicChest[Main.tile[x, y].TileType])
                             {
-                                if (Main.tile[x, y].TileFrameX % 36 != 0)
-                                {
-                                    x--;
-                                }
-                                if (Main.tile[x, y].TileFrameY % 36 != 0)
-                                {
-                                    y--;
-                                }
+                                if (Main.tile[x, y].TileFrameX % 36 != 0) x--;
+                                if (Main.tile[x, y].TileFrameY % 36 != 0) y--;
 
-                                for (int l = 0; l < 40; l++)
-                                {
-                                    Main.chest[num].item[l].TurnToAir(true);
-                                }
+                                for (int l = 0; l < 40; l++) lastChest.item[l] = new Item();
 
+                                Chest.DestroyChest(x, y);
                                 for (int j = x; j <= x + 1; j++)
                                 {
                                     for (int k = y; k <= y + 1; k++)
                                     {
                                         if (TileID.Sets.BasicChest[Main.tile[j, k].TileType])
                                         {
-                                            Tile chest = Main.tile[j, k];
-                                            chest.HasTile = false;
+                                            Main.tile[j, k].ClearTile();
                                         }
                                     }
                                 }
 
-                                Chest.DestroyChest(x, y);
-                                NetMessage.SendData(MessageID.ChestUpdates, -1, -1, null, 1, x, y, 0f, Chest.FindChest(x, y));
+                                int chestID = 1;
+                                if (Main.tile[x, y].TileType == TileID.Containers2) chestID = 5;
+                                if (Main.tile[x, y].TileType >= TileID.Count) chestID = 101;
+
+                                NetMessage.SendData(MessageID.ChestUpdates, -1, -1, null, chestID, x, y, 0, Chest.FindChest(x, y));
                                 NetMessage.SendTileSquare(-1, x, y, 3);
                             }
+
                             short mimicID = WorldGen.crimson ? NPCID.BigMimicCorruption : NPCID.BigMimicCrimson;
-                            int npcIndex = NPC.NewNPC(Terraria.Entity.GetSource_NaturalSpawn(), x * 16 + 16, y * 16 + 32, mimicID);
+                            int npcIndex = NPC.NewNPC(Entity.GetSource_TileInteraction(x, y), x * 16, y * 16 + 32, mimicID);
                             Main.npc[npcIndex].whoAmI = npcIndex;
-                            NetMessage.SendData(MessageID.SyncNPC, -1, -1, null, npcIndex);
+                            NetMessage.SendData(MessageID.SyncNPC, number : npcIndex);
                             Main.npc[npcIndex].BigMimicSpawnSmoke();
                         }
                     }
@@ -91,10 +80,7 @@ namespace InfectedQualities.Core
             }
         }
 
-        public override void UpdateAutopause()
-        {
-            LastChest = Player.chest;
-        }
+        public override void UpdateAutopause() => LastChest = Player.chest;
 
         public override bool IsLoadingEnabled(Mod mod) => ModContent.GetInstance<InfectedQualitiesServerConfig>().KeyOfNaught;
     }
